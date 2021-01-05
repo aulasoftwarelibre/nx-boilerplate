1eac0d4e47e18f7c349d2e85554486165de80b4e
Sergio Gómez <sergio@uco.es>
2021-01-05 12:43:54 +0100

    fix: next-auth mock

--- apps/web/specs/index.spec.tsx
+++ apps/web/specs/index.spec.tsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
+import client, { Session } from "next-auth/client";
 import React from 'react';
 
 import Index from '../pages/index';
 
+jest.mock("next-auth/client");
+
 describe('Index', () => {
   it('should render successfully', () => {
+    const mockSession: Session = {
+      expires: "1",
+      user: { email: "john@doe.com", name: "John Doe" },
+    };
+
+    (client.useSession as jest.Mock).mockReturnValueOnce([mockSession, false]);
+
     const { baseElement } = render(<Index />);
     expect(baseElement).toBeTruthy();
   });
